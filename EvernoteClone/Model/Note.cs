24cad8db1f7868811dc4f7f4f8aeb65a8c8911d0
Jose Alonso <mail@josealonso.dev>
2022-08-02 23:49:35 -0500

    Adding SQLite and a Database helper

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using SQLite;
 
 namespace EvernoteClone.Model;
 
 public class Note
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    [Indexed]
     public int NotebookId { get; set; }
     public string Title { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
---
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SQLite;
+
+namespace EvernoteClone.ViewModel.Helpers;
+
+public class DatabaseHelper
+{
+    private static string _dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+    public static bool Insert<T>(T item)
+    {
+        
+
+        using var conn = new SQLiteConnection(_dbFile);
+        conn.CreateTable<T>();
+
+        var rows = conn.Insert(item);
+
+        return rows > 0;
+    }
+
+    public static bool Update<T>(T item)
+    {
+
+
+        using var conn = new SQLiteConnection(_dbFile);
+        conn.CreateTable<T>();
+
+        var rows = conn.Update(item);
+
+        return rows > 0;
+    }
+
+    public static bool Delete<T>(T item)
+    {
+
+
+        using var conn = new SQLiteConnection(_dbFile);
+        conn.CreateTable<T>();
+
+        var rows = conn.Delete(item);
+
+        return rows > 0;
+    }
+
+    public static List<T> Read<T>() where T : new()
+    {
+        using var conn = new SQLiteConnection(_dbFile);
+        conn.CreateTable<T>();
+
+        var items = conn.Table<T>().ToList();
+
+        return items;
+    }
+}
