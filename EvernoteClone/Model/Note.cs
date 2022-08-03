9f428a23de72249e59668566e81719f736eac956
Jose Alonso <mail@josealonso.dev>
2022-08-02 23:23:27 -0500

    Add project files.

---
+++ EvernoteClone/Model/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EvernoteClone.Model;
+
+public class Note
+{
+    public int Id { get; set; }
+    public int NotebookId { get; set; }
+    public string Title { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public string FileLocation { get; set; }
+}
---
+++ EvernoteClone/Model/Notebook.cs
@@ -0,0 +1,8 @@
+﻿namespace EvernoteClone.Model;
+
+public class Notebook
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Name { get; set; }
+}
